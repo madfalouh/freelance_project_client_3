2752458f54eebaffd9c34ca28dfcb3ce11c87836
madfalouh <70645288+madfalouh@users.noreply.github.com>
2022-07-06 14:51:58 +0300

    added globe

---
+++ freelance/index.js
@@ -0,0 +1,181 @@
+import ThreeGlobe from 'three-globe';
+import { WebGLRenderer, Scene } from "three";
+import {
+  PerspectiveCamera,
+  AmbientLight,
+  DirectionalLight,
+  Color,
+  Fog,
+  // AxesHelper,
+  // DirectionalLightHelper,
+  // CameraHelper,
+  PointLight,
+  SphereGeometry,
+} from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import countries from "./files/globe-data-min.json";
+import travelHistory from "./files/my-flights.json";
+import airportHistory from "./files/my-airports.json";
+var renderer, camera, scene, controls;
+let mouseX = 0;
+let mouseY = 0;
+let windowHalfX = window.innerWidth / 2;
+let windowHalfY = window.innerHeight / 2;
+var Globe;
+
+init();
+initGlobe();
+onWindowResize();
+animate();
+
+// SECTION Initializing core ThreeJS elements
+function init() {
+  // Initialize renderer
+  renderer = new WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // renderer.outputEncoding = THREE.sRGBEncoding;
+  document.body.appendChild(renderer.domElement);
+
+  // Initialize scene, light
+  scene = new Scene();
+  scene.add(new AmbientLight(0xbbbbbb, 0.3));
+  scene.background = new Color(0x040d21);
+
+  // Initialize camera, light
+  camera = new PerspectiveCamera();
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  var dLight = new DirectionalLight(0xffffff, 0.8);
+  dLight.position.set(-800, 2000, 400);
+  camera.add(dLight);
+
+  var dLight1 = new DirectionalLight(0x7982f6, 1);
+  dLight1.position.set(-200, 500, 200);
+  camera.add(dLight1);
+
+  var dLight2 = new PointLight(0x8566cc, 0.5);
+  dLight2.position.set(-200, 500, 200);
+  camera.add(dLight2);
+
+  camera.position.z = 300;
+  camera.position.x = 0;
+  camera.position.y = 0;
+
+  scene.add(camera);
+
+  // Additional effects
+  scene.fog = new Fog(0x535ef3, 400, 2000);
+
+  // Helpers
+  // const axesHelper = new AxesHelper(800);
+  // scene.add(axesHelper);
+  // var helper = new DirectionalLightHelper(dLight);
+  // scene.add(helper);
+  // var helperCamera = new CameraHelper(dLight.shadow.camera);
+  // scene.add(helperCamera);
+
+  // Initialize controls
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.dynamicDampingFactor = 0.01;
+  controls.enablePan = false;
+
+  controls.rotateSpeed = 0.8;
+  controls.zoomSpeed = 1;
+  controls.autoRotate = false;
+
+  controls.minPolarAngle = Math.PI / 3.5;
+  controls.maxPolarAngle = Math.PI - Math.PI / 3;
+
+  window.addEventListener("resize", onWindowResize, false);
+  document.addEventListener("mousemove", onMouseMove);
+}
+
+// SECTION Globe
+function initGlobe() {
+  // Initialize the Globe
+  Globe = new ThreeGlobe({
+    waitForGlobeReady: true,
+    animateIn: true,
+  })
+    .hexPolygonsData(countries.features)
+    .hexPolygonResolution(3)
+    .hexPolygonMargin(0.7)
+    .showAtmosphere(true)
+    .atmosphereColor("#3a228a")
+    .atmosphereAltitude(0.25)
+    .hexPolygonColor((e) => {
+      if (
+        ["MAR","KGZ", "KOR", "THA", "RUS", "UZB", "IDN", "KAZ", "MYS"].includes(
+          e.properties.ISO_A3
+        )
+      ) {
+        return "rgba(255,255,255, 1)";
+      } else return "rgba(255,255,255, 0.7)";
+    });
+
+  // NOTE Arc animations are followed after the globe enters the scene
+  setTimeout(() => {
+    Globe.arcsData(travelHistory.flights)
+        
+      .arcColor((e) => {
+        
+        return  "#"+Math.floor(Math.random()*16777215).toString(16);
+      })
+      .arcAltitude((e) => {
+        return e.arcAlt;
+      })
+      .arcStroke((e) => {
+        return e.status ? 0.5 : 0.3;
+      })
+      .arcDashLength(1.2)
+      .arcDashGap(4)
+      
+      .arcDashAnimateTime(1000)
+      .arcsTransitionDuration(1000)
+      .arcDashInitialGap((e) => e.order * 1)
+      .pointsData(airportHistory.airports)
+      .pointColor(() => "#ffffff")
+      .pointsMerge(true)
+      .pointAltitude(0.07)
+      .pointRadius(0.05);
+  }, 1000);
+
+  Globe.rotateY(-Math.PI * (5 / 9));
+  Globe.rotateZ(-Math.PI / 6);
+  const globeMaterial = Globe.globeMaterial();
+  globeMaterial.color = new Color(0x000080);
+  globeMaterial.emissive = new Color(0x2200);
+  globeMaterial.emissiveIntensity = 0.1;
+  globeMaterial.shininess = 0.7;
+
+  // NOTE Cool stuff
+  // globeMaterial.wireframe = true;
+
+  scene.add(Globe);
+}
+
+function onMouseMove(event) {
+  mouseX = event.clientX - windowHalfX;
+  mouseY = event.clientY - windowHalfY;
+  // console.log("x: " + mouseX + " y: " + mouseY);
+}
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  windowHalfX = window.innerWidth / 1.5;
+  windowHalfY = window.innerHeight / 1.5;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+  Globe.rotateY(0.003)
+  camera.lookAt(scene.position);
+  controls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
