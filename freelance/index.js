8bf6a6632bf83726f20b17e3b799aaa77d6006ce
madfalouh <70645288+madfalouh@users.noreply.github.com>
2022-07-07 20:06:32 +0300

    added tree line

---
+++ freelance/index.js
@@ -0,0 +1,105 @@
+import * as THREE from 'three'
+
+
+import ThreeGlobe from 'three-globe';
+import { WebGLRenderer, Scene } from "three";
+import {
+  PerspectiveCamera,
+  AmbientLight,
+  DirectionalLight,
+  Color,
+  Fog,
+  // AxesHelper,
+  // DirectionalLightHelper,
+  // CameraHelper,
+  PointLight,
+  SphereGeometry,
+} from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import countries from "./files/globe-data-min.json";
+import travelHistory from "./files/my-flights.json";
+import airportHistory from "./files/my-airports.json";
+var renderer, camera, scene, controls;
+let mouseX = 0;
+let mouseY = 0;
+let windowHalfX = window.innerWidth / 2;
+let windowHalfY = window.innerHeight / 2;
+var Globe;
+
+init();
+animate();
+
+// SECTION Initializing core ThreeJS elements
+function init() {
+  // Initialize renderer
+  renderer = new WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // renderer.outputEncoding = THREE.sRGBEncoding;
+  document.body.appendChild(renderer.domElement);
+
+  // Initialize scene, light
+  scene = new Scene();
+  scene.add(new AmbientLight(0xbbbbbb, 0.3));
+  scene.background = new Color(0x040d21);
+
+  // Initialize camera, light
+  camera = new PerspectiveCamera();
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  var dLight = new DirectionalLight(0xffffff, 0.8);
+  dLight.position.set(-800, 2000, 400);
+  camera.add(dLight);
+
+  var dLight1 = new DirectionalLight(0x7982f6, 1);
+  dLight1.position.set(-200, 500, 200);
+  camera.add(dLight1);
+
+  var dLight2 = new PointLight(0x8566cc, 0.5);
+  dLight2.position.set(-200, 500, 200);
+  camera.add(dLight2);
+
+  camera.position.z = 300;
+  camera.position.x = 0;
+  camera.position.y = 0;
+  scene.add(camera);
+  scene.fog = new Fog(0x535ef3, 400, 2000);
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.dynamicDampingFactor = 0.01;
+  controls.enablePan = false;
+
+  controls.rotateSpeed = 0.8;
+  controls.zoomSpeed = 1;
+  controls.autoRotate = false;
+
+  controls.minPolarAngle = Math.PI / 3.5;
+  controls.maxPolarAngle = Math.PI - Math.PI / 3;
+
+
+const curve = new THREE.SplineCurve( [
+
+	new THREE.Vector2( 0, 0 ),
+	new THREE.Vector2( 0, 0 ),
+	new THREE.Vector2( 50, -90 ),
+	new THREE.Vector2( 200, -10 )
+] );
+
+const points = curve.getPoints( 250 );
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+const material = new THREE.LineBasicMaterial( { color: 0xff0000  , linewidth:50} );
+
+// Create the final object to add to the scene
+const splineObject = new THREE.Line( geometry, material );
+
+scene.add(splineObject)
+}
+
+
+function animate() {
+  controls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
